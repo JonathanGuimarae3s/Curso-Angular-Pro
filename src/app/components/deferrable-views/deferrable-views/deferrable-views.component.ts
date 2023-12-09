1babe2e2a8e909faa4a1f7ca0f5ea95eb253bc97
Jonathan  Guimarães <guimaraesjonathan92@gmail.com>
2023-12-09 17:04:50 -0300

    signals

--- src/app/components/deferrable-views/deferrable-views/deferrable-views.component.ts
+++ src/app/components/deferrable-views/deferrable-views/deferrable-views.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { NewComponentComponent } from '../../new-component/new-component.component';
+import { Observable, of, delay } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-deferrable-views',
   standalone: true,
-  imports: [NewComponentComponent],
+  imports: [NewComponentComponent, CommonModule],
   templateUrl: './deferrable-views.component.html',
   styleUrl: './deferrable-views.component.scss'
 })
 export class DeferrableViewsComponent {
 
+
+  public isTrue = true;
+  public loadingData: Observable<string[]> = of(['item 1', 'item 2', 'item 3']).pipe(delay(3000));
 }
